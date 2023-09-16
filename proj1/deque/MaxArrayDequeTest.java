078f125506124ba3365a921a3ee03e22fc13969f
Cngo2019 <canngo215@gmail.com>
2023-09-16 15:06:29 -0400

    Add another unit test

--- proj1/deque/MaxArrayDequeTest.java
+++ proj1/deque/MaxArrayDequeTest.java
@@ -21,7 +21,7 @@ public class MaxArrayDequeTest {
     }
 
     @Test
-    public void test_findMaxStringAfterResizing() {
+    public void test_findMaxStringAfterUpSizing() {
         MaxArrayDeque<String> maxDeque = new MaxArrayDeque(new StringComparator());
         maxDeque.addFirst("A");
         maxDeque.addFirst("A");
@@ -36,6 +36,29 @@ public class MaxArrayDequeTest {
         maxDeque.addLast("D");
         maxDeque.addLast("B");
         maxDeque.addFirst("F");
+        assertEquals(maxDeque.max(), "Z");
+    }
+
+    @Test
+    public void test_findMaxStringAfterDesizing() {
+        MaxArrayDeque<String> maxDeque = new MaxArrayDeque(new StringComparator());
+        maxDeque.addFirst("A");
+        maxDeque.addFirst("A");
+        maxDeque.addLast("D");
+        maxDeque.addLast("B");
+        maxDeque.addFirst("A");
+        maxDeque.addFirst("A");
+        maxDeque.addLast("D");
+        maxDeque.addLast("B");
+        maxDeque.addFirst("A");
+        maxDeque.addFirst("Z");
+        maxDeque.addLast("D");
+        maxDeque.addLast("B");
+        maxDeque.addFirst("F");
+        for (int i = 0; i < 11; i++) {
+            maxDeque.removeLast();
+        }
+        maxDeque.addLast("Z");
         assertEquals(maxDeque.max(), "Z");
     }
 }
